7162e3b5ac0c459c074ea9c99953813ed9da8ebd
Vera Kippel <veki2400@student.miun.se>
2025-05-02 20:45:47 +0200

    lägger in nya komponenter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'calculate', component: CalculateComponent },
+    { path: 'about', component: AboutComponent }
+];
---
+++ src/app/calculate/calculate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate',
+  imports: [],
+  templateUrl: './calculate.component.html',
+  styleUrl: './calculate.component.scss'
+})
+export class CalculateComponent {
+
+}
