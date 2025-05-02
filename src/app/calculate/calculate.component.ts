5dd3576bf0d031717397b8355d8e9e1de0d27a5e
Vera Kippel <veki2400@student.miun.se>
2025-05-02 22:23:15 +0200

    lägger in konverteringar

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculate',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculate.component.html',
   styleUrl: './calculate.component.scss'
 })
 export class CalculateComponent {
+  //properties
+  fahr: number = 0;
+  cels: number = 0;
+  resultTemp: number = 0;
+  
+  meter: number = 0;
+  feet: number = 0;
+  resultLength = 0;
 
+  //methods
+  fahrToCels(): void{
+    this.resultTemp = (this.fahr - 32) / 1.8;
+  }
+  celsToFahr(): void{
+    this.resultTemp = (this.cels * 1.8) + 32;
+  }
+
+  meterToFeet(): void{
+    this.resultLength = this.meter * 3.28084;
+  }
+  feetToMeter(): void{
+    this.resultLength = this.feet / 3.28084;
+  }
 }
