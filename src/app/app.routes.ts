ced92729b605a1232da370ec918363f5b57fce51
Vera Kippel <veki2400@student.miun.se>
2025-05-02 20:51:25 +0200

    lägger till notfound -sida och lägger till i routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalculateComponent } from './calculate/calculate.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'calculate', component: CalculateComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '404', component: NotFoundComponent },
+    { path: '**', component: NotFoundComponent}
 ];
